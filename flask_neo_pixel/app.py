2b82562a457d4507b95d40ea37f81af878821d0a
JumboBumbo <dwheels91@gmail.com>
2021-04-03 13:00:03 +0100

    set_all complete, not impl msg set_px

--- flask_neo_pixel/app.py
+++ flask_neo_pixel/app.py
@@ -18,45 +18,53 @@ BRIGHTNESS = 0.8
 AUTO_WRITE = False
 
 # OTHER DEFAULTS
-brightness_key = "brightness"
+BRIGHTNESS_KEY = "brightness"
 
 app = Flask(__name__, static_url_path="") # flask object
 
 
 def _post_data() -> dict:
-    """[summary]
+    """The json data sent via a POST
 
     Returns:
-        dict: [description]
+        dict: The POST data
     """
     post_data = request.get_json()
-    if brightness_key not in post_data.keys():
-        post_data[brightness_key] = BRIGHTNESS
+    if BRIGHTNESS_KEY not in post_data.keys():
+        post_data[BRIGHTNESS_KEY] = BRIGHTNESS
 
     return post_data
 
 
 def _create_neo_pixel(brightness: float) -> object:
-    """[summary]
+    """Due to the below constraints of the NeoPixel implementation, a with block/constant are not possible:
+
+      - When the with block is exited, the NeoPixel object is deleted, setting the lighs to 0 (off)
+      - The brightness level is set when creating the object, this needs to remain customisable via flask
 
     Args:
-        brightness (float): [description]
+        brightness (float): 0 - 1.0
 
     Returns:
-        object: [description]
+        object: The NeoPixel object
     """
     return neopixel.NeoPixel(PX_PIN, NUM_PX, auto_write=AUTO_WRITE, pixel_order=ORDER, brightness=brightness)
 
 
 @app.route("/set_px", methods=["POST"])
 def set_px() -> str:
-    pass
+    """NOT YET IMPLEMENTED
+
+    Returns:
+        str: json string
+    """
+    return jsonify({"success": False, "error": "Not implemented"})
 
 
 set_all_schema = {
     "properties": {
         "rgb": {"type": "array"},
-        brightness_key: {"type": "number"}
+        BRIGHTNESS_KEY: {"type": "number"}
     },
     "required": ["rgb"]
 }
@@ -67,13 +75,13 @@ def set_all() -> str:
 
     Returns:
         str: json string
-
     """
     return_resp = {}
     post_data = _post_data()
+
     try:
 
-        neo = _create_neo_pixel(post_data[brightness_key])
+        neo = _create_neo_pixel(post_data[BRIGHTNESS_KEY])  # neo object
         neo.fill(post_data["rgb"])
         neo.show()
         return_resp["success"] = True
